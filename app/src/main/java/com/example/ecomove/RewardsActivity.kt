f9e5c90c6b5d79538577b996891a15473b6e8d2d
Marcelo rentería <u202216558@upc.edu.pe>
2024-09-25 19:38:11 -0500

    feat: Added Support, Alert, Publish and Rewards Activities.

---
+++ app/src/main/java/com/example/ecomove/RewardsActivity.kt
@@ -0,0 +1,26 @@
+package com.example.ecomove
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class RewardsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_rewards)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val rvReward = findViewById<RecyclerView>(R.id.rvRewards)
+        rvReward.layoutManager=LinearLayoutManager(this@RewardsActivity)
+
+    }
+}
